62666dd6d15ac23d2dc44245247892332de44d26
PriyanshuM04 <priyanshumallick04@gmail.com>
2025-07-18 22:24:42 +0530

    Merged Routes Datasets

---
+++ merge_routes.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+routes_folder = "Routes"
+output_folder = "MasterData"
+output_file = "master_routes.csv"
+
+os.makedirs(output_folder, exist_ok=True)
+
+csv_files = [file for file in os.listdir(routes_folder) if file.endswith(".csv")]
+
+all_data = []
+
+for file in csv_files:
+    file_path = os.path.join(routes_folder, file)
+    try:
+        df = pd.read_csv(file_path)
+        # df['TrainNo'] = file.replace(".csv", "")
+        all_data.append(df)
+    except Exception as e:
+        print(f"⚠️ Could not read {file}: {e}")
+
+# === Merge all DataFrames ===
+merged_df = pd.concat(all_data, ignore_index=True)
+
+merged_df.to_csv(os.path.join(output_folder, output_file), index=False)
+print(f"✅ Merged {len(csv_files)} files into '{output_file}'")
